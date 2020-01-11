97dacd81a376dac1ae5c7c258ffe18a21666fad5
dteiml <dteiml@gmail.com>
2020-01-11 08:58:08 +0100

    project structure

---
+++ src/components/home/home-view.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const View = () => do {
+  <div>Hello</div>
+}
+
+export default styled (View) ``
---
+++ src/components/layout/layout-view.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const View = (props) => do {
+  /* eslint-disable semi */
+  const {children} = props;
+  <div>{children}</div>
+}
+
+export default styled (View) ``
